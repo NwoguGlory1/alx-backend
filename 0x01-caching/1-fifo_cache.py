e24e33f4537e8eabe5016ebd617cf651988efd6a
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-22 22:16:56 +0000

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,21 +10,28 @@ class FIFOCache(BaseCaching):
         """ Constructor """
         super().__init__()
         self.order = []
+        """ keeps track of the order in which keys are added to cache"""
 
     def put(self, key, item):
-        """ method that add an item in the cache """
+        """ method that add a new item in the cache """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
             self.order.remove(key)
             """ remove key if it already exists"""
+        # self.cache_data is a dictionary. It stores the cached items
+        # Each item is associated with a unique key.
         self.cache_data[key] = item
+        # Add the new key-value pair to the cache
         self.order.append(key)
+        # Add the key to the order list
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             oldest_firstkey = self.order.pop(0)
+            # Remove oldest key
             del self.cache_data[oldest_firstkey]
+            # Remove oldest key value pair
             print(f"DISCARD: {oldest_firstkey}")
 
     def get(self, key):
