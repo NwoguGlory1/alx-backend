4c4971c48005af970e8a86867cf796c819689837
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-22 17:47:37 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Basic dictionary script """
+from base_caching import BaseCaching
+""" """
+
+
+class BasicCache(BaseCaching):
+    """ class that inherits from BaseCaching and is a caching system """
+    def put(self, key, item):
+        """ method that add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ method that get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
