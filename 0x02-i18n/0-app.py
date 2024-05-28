299ce7e856e62ee7f04af27cf24cfe0b0fa5a8c8
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-28 23:47:57 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-""" """
+""" Basic Flask app script"""
+
 from flask import Flask, render_template
-""" """
+""" imports necessary module"""
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+"""
+defines a root in Flask app,
+'/' will be accessed when user vivits root URL
+"""
+def index() -> str:
+    """ function that will be called when user visits root URL """
     return render_template('index.html')
 
 if __name__ == '__main__':
