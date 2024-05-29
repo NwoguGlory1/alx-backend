fee1a67050f4921de5d9a8ce261ec3df5b7b9682
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-29 04:31:28 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-""" Basic Flask app script"""
+
+""" Module for trying out Babel i18n """
 
 from flask import Flask, render_template
-""" imports necessary module"""
 
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-"""
-defines a root in Flask app,
-'/' will be accessed when user vivits root URL
-"""
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("0-index.html")
 
 
-def index() -> str:
-    """ function that will be called when user visits root URL """
-    return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
