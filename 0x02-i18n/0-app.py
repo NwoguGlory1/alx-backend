f3247af662343cefcba3bcc65ade383880ac743f
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-29 04:51:08 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@
 from flask import Flask, render_template
 """ imports necessary module"""
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
