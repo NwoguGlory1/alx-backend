758fc16761f021f7a4a74a06aa8bcb3d0abf8eaa
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-29 04:50:11 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+""" Module for trying out Babel i18n """
+
+from flask_babel import Babel
+from flask import Flask, render_template
+""" imports necessary module"""
+
+class Config:
+    """ decalring a class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
