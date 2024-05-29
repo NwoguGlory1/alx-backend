5ec587a2bed127326db91a460cd7e92e9c356f05
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-29 04:35:59 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,12 +3,13 @@
 """ Module for trying out Babel i18n """
 
 from flask import Flask, render_template
+""" imports necessary module"""
 
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
+def index() -> str:
     """Renders a Basic Template for Babel Implementation"""
     return render_template("0-index.html")
 
