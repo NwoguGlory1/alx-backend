973b3aedaa08d816ff06f9260d7a6fd446ff1ed6
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-25 16:43:41 +0000

    Edited some files

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,6 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const client = redis.createClient()
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
