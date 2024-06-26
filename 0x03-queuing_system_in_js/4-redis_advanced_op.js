12505a8ad827968486300598ddc550431ca7c680
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-26 00:41:28 +0000

    Node Redis client and async operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -17,3 +17,7 @@ client.hset('HolbertonSchools', 'New York', '20', redis.print);
 client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
 client.hset('HolbertonSchools', 'Cali', '40', redis.print);
 client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+
+client.hgetall('HolbertonSchools', (err, value) => {
+  console.log(value);
+});
