d5137c6f140e65bb8540e4c0679dd832bfa5d4ea
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-27 12:56:12 +0000

    Create the Job creator

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -13,11 +13,15 @@ const queueName = 'push_notification_code';
 const job = queue.create(queueName, object_jobdata).save((err) => {
   if (!err) {
     console.log(`Notification job created: ${job.id}`);
+  } else {
+    console.log(`Failed to create job: ${err}`);
+  }
 });
+
 job.on('complete', () => {
-    console.log('Notification job completed');
-  });
+  console.log('Notification job completed');
+});
 
 job.on('failed', () => {
-    console.log(`Notification job failed`)
+  console.log('Notification job failed');
 });
