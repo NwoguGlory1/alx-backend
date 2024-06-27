c9ed3416e6105c7697f60392b23bc01024851de3
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-27 10:43:47 +0000

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -9,3 +9,14 @@ client.on('connect', () => {
 client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
+
+function publishMessage(message, time) {
+  setTimeout(() => {
+    client.publish("holberton school channel", message);
+    console.log(`About to send ${message}`);
+  }, time);
+}
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -10,5 +10,17 @@ client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
-client.subscribe('holberton school channel'){
-  console.log(``)
+// Step 2: Subscribe to the channel
+const channel = 'holberton school channel';
+client.subscribe(channel);
+
+// Step 3: Handle incoming messages
+client.on('message', (channel, message) => {
+  console.log(`Received message from ${channel}: ${message}`);
+
+// Step 4: Graceful shutdown on receiving 'KILL_SERVER'
+if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel, () => {
+    client.quit();
+  });
+}
